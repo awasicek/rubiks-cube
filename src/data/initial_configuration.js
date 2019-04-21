162d3bd442513864a4ae361bfca52fcaaffd762b
Andrew Wasicek <awasicek@gmail.com>
2019-04-21 00:40:50 -0700

    rotate middle 90 degree right prototyped

--- src/data/initial_configuration.js
+++ src/data/initial_configuration.js
@@ -1,7 +1,7 @@
 import COLORS from "@constants/colors";
 import FACES from "@constants/faces";
 /**
- * @fileoverview Array of tile objects that represent the initial configuration of the Rubik's cube.
+ * @fileoverview Array of tile objects seed data for a map that represent the initial configuration of the Rubik's cube.
  */
 
 // TODO -- consider another data structure arrangement such as sorting by faces for better access times
@@ -15,74 +15,101 @@ const INIT_CONFIG = [
     // bottom row, left to right
     //--------------------------------------
     // tile 1
+    // ["1", {
     {
         face: FACES.FACE_A,
         "x-coordinate": 0,
         "y-coordinate": 0,
-        color: COLORS.BLUE
+        color: COLORS.BLUE,
+        uid: 1
     },
+    // }],
     // tile 2
+    // ["2", {
     {
         face: FACES.FACE_A,
         "x-coordinate": 1,
         "y-coordinate": 0,
-        color: COLORS.BLUE
+        color: COLORS.BLUE,
+        uid: 2
     },
+    // }],
     // tile 3
+    // ["3", {
     {
         face: FACES.FACE_A,
         "x-coordinate": 2,
         "y-coordinate": 0,
-        color: COLORS.BLUE
+        color: COLORS.BLUE,
+        uid: 3
     },
+    // }],
     //--------------------------------------
     // middle row, left to right
     //--------------------------------------
     // tile 4
+    // ["4", {
     {
         face: FACES.FACE_A,
         "x-coordinate": 0,
         "y-coordinate": 1,
-        color: COLORS.BLUE
+        color: COLORS.BLUE,
+        uid: 4
     },
+    // }],
     // tile 5
+    // ["5", {
     {
         face: FACES.FACE_A,
         "x-coordinate": 1,
         "y-coordinate": 1,
-        color: COLORS.BLUE
+        color: COLORS.BLUE,
+        uid: 5
     },
+    // }],
     // tile 6
+    // ["6", {
     {
         face: FACES.FACE_A,
         "x-coordinate": 2,
         "y-coordinate": 1,
-        color: COLORS.BLUE
+        color: COLORS.BLUE,
+        uid: 6
     },
+    // }],
     //--------------------------------------
     // top row, left to right
     //--------------------------------------
     // tile 7
+    // ["7", {
     {
         face: FACES.FACE_A,
         "x-coordinate": 0,
         "y-coordinate": 2,
-        color: COLORS.BLUE
+        color: COLORS.BLUE,
+        uid: 7
     },
+    // }],
     // tile 8
+    // ["8", {
     {
         face: FACES.FACE_A,
         "x-coordinate": 1,
         "y-coordinate": 2,
-        color: COLORS.BLUE
+        color: COLORS.BLUE,
+        uid: 8
     },
+    // }],
     // tile 9
+    // ["9", {
     {
         face: FACES.FACE_A,
         "x-coordinate": 2,
         "y-coordinate": 2,
-        color: COLORS.BLUE
+        color: COLORS.BLUE,
+        uid: 9
     },
+    // }],
 
     //======================================
     // FACE B (initially the back face)
@@ -92,74 +119,101 @@ const INIT_CONFIG = [
     // bottom row, left to right
     //--------------------------------------
     // tile 1
+    // ["10", {
     {
         face: FACES.FACE_B,
         "x-coordinate": 0,
         "y-coordinate": 0,
-        color: COLORS.RED
+        color: COLORS.RED,
+        uid: 10
     },
+    // }],
     // tile 2
+    // ["11", {
     {
         face: FACES.FACE_B,
         "x-coordinate": 1,
         "y-coordinate": 0,
-        color: COLORS.RED
+        color: COLORS.RED,
+        uid: 11
     },
+    // }],
     // tile 3
+    // ["12", {
     {
         face: FACES.FACE_B,
         "x-coordinate": 2,
         "y-coordinate": 0,
-        color: COLORS.RED
+        color: COLORS.RED,
+        uid: 12
     },
+    // }],
     //--------------------------------------
     // middle row, left to right
     //--------------------------------------
     // tile 4
+    // ["13", {
     {
         face: FACES.FACE_B,
         "x-coordinate": 0,
         "y-coordinate": 1,
-        color: COLORS.RED
+        color: COLORS.RED,
+        uid: 13
     },
+    // }],
     // tile 5
+    // ["14", {
     {
         face: FACES.FACE_B,
         "x-coordinate": 1,
         "y-coordinate": 1,
-        color: COLORS.RED
+        color: COLORS.RED,
+        uid: 14
     },
+    // }],
     // tile 6
+    // ["15", {
     {
         face: FACES.FACE_B,
         "x-coordinate": 2,
         "y-coordinate": 1,
-        color: COLORS.RED
+        color: COLORS.RED,
+        uid: 15
     },
+    // }],
     //--------------------------------------
     // top row, left to right
     //--------------------------------------
     // tile 7
+    // ["16", {
     {
         face: FACES.FACE_B,
         "x-coordinate": 0,
         "y-coordinate": 2,
-        color: COLORS.RED
+        color: COLORS.RED,
+        uid: 16
     },
+    // }],
     // tile 8
+    // ["17", {
     {
         face: FACES.FACE_B,
         "x-coordinate": 1,
         "y-coordinate": 2,
-        color: COLORS.RED
+        color: COLORS.RED,
+        uid: 17
     },
+    // }],
     // tile 9
+    // ["18", {
     {
         face: FACES.FACE_B,
         "x-coordinate": 2,
         "y-coordinate": 2,
-        color: COLORS.RED
+        color: COLORS.RED,
+        uid: 18
     },
+    // }],
 
     //======================================
     // FACE C (initially the top face)
@@ -169,74 +223,101 @@ const INIT_CONFIG = [
     // bottom row, left to right
     //--------------------------------------
     // tile 1
+    // ["19", {
     {
         face: FACES.FACE_C,
         "x-coordinate": 0,
         "y-coordinate": 0,
-        color: COLORS.GREEN
+        color: COLORS.GREEN,
+        uid: 19
     },
+    // }],
     // tile 2
+    // ["20", {
     {
         face: FACES.FACE_C,
         "x-coordinate": 1,
         "y-coordinate": 0,
-        color: COLORS.GREEN
+        color: COLORS.GREEN,
+        uid: 20
     },
+    // }],
     // tile 3
+    // ["21", {
     {
         face: FACES.FACE_C,
         "x-coordinate": 2,
         "y-coordinate": 0,
-        color: COLORS.GREEN
+        color: COLORS.GREEN,
+        uid: 21
     },
+    // }],
     //--------------------------------------
     // middle row, left to right
     //--------------------------------------
     // tile 4
+    // ["22", {
     {
         face: FACES.FACE_C,
         "x-coordinate": 0,
         "y-coordinate": 1,
-        color: COLORS.GREEN
+        color: COLORS.GREEN,
+        uid: 22
     },
+    // }],
     // tile 5
+    // ["23", {
     {
         face: FACES.FACE_C,
         "x-coordinate": 1,
         "y-coordinate": 1,
-        color: COLORS.GREEN
+        color: COLORS.GREEN,
+        uid: 23
     },
+    // }],
     // tile 6
+    // ["24", {
     {
         face: FACES.FACE_C,
         "x-coordinate": 2,
         "y-coordinate": 1,
-        color: COLORS.GREEN
+        color: COLORS.GREEN,
+        uid: 24
     },
+    // }],
     //--------------------------------------
     // top row, left to right
     //--------------------------------------
     // tile 7
+    // ["25", {
     {
         face: FACES.FACE_C,
         "x-coordinate": 0,
         "y-coordinate": 2,
-        color: COLORS.GREEN
+        color: COLORS.GREEN,
+        uid: 25
     },
+    // }],
     // tile 8
+    // ["26", {
     {
         face: FACES.FACE_C,
         "x-coordinate": 1,
         "y-coordinate": 2,
-        color: COLORS.GREEN
+        color: COLORS.GREEN,
+        uid: 26
     },
+    // }],
     // tile 9
+    // ["27", {
     {
         face: FACES.FACE_C,
         "x-coordinate": 2,
         "y-coordinate": 2,
-        color: COLORS.GREEN
+        color: COLORS.GREEN,
+        uid: 27
     },
+    // }],
 
     //======================================
     // FACE D (initially the bottom face)
@@ -246,74 +327,101 @@ const INIT_CONFIG = [
     // bottom row, left to right
     //--------------------------------------
     // tile 1
+    // ["28", {
     {
         face: FACES.FACE_D,
         "x-coordinate": 0,
         "y-coordinate": 0,
-        color: COLORS.ORANGE
+        color: COLORS.ORANGE,
+        uid: 28
     },
+    // }],
     // tile 2
+    // ["29", {
     {
         face: FACES.FACE_D,
         "x-coordinate": 1,
         "y-coordinate": 0,
-        color: COLORS.ORANGE
+        color: COLORS.ORANGE,
+        uid: 29
     },
+    // }],
     // tile 3
+    // ["30", {
     {
         face: FACES.FACE_D,
         "x-coordinate": 2,
         "y-coordinate": 0,
-        color: COLORS.ORANGE
+        color: COLORS.ORANGE,
+        uid: 30
     },
+    // }],
     //--------------------------------------
     // middle row, left to right
     //--------------------------------------
     // tile 4
+    // ["31", {
     {
         face: FACES.FACE_D,
         "x-coordinate": 0,
         "y-coordinate": 1,
-        color: COLORS.ORANGE
+        color: COLORS.ORANGE,
+        uid: 31
     },
+    // }],
     // tile 5
+    // ["32", {
     {
         face: FACES.FACE_D,
         "x-coordinate": 1,
         "y-coordinate": 1,
-        color: COLORS.ORANGE
+        color: COLORS.ORANGE,
+        uid: 32
     },
+    // }],
     // tile 6
+    // ["33", {
     {
         face: FACES.FACE_D,
         "x-coordinate": 2,
         "y-coordinate": 1,
-        color: COLORS.ORANGE
+        color: COLORS.ORANGE,
+        uid: 33
     },
+    // }],
     //--------------------------------------
     // top row, left to right
     //--------------------------------------
     // tile 7
+    // ["34", {
     {
         face: FACES.FACE_D,
         "x-coordinate": 0,
         "y-coordinate": 2,
-        color: COLORS.ORANGE
+        color: COLORS.ORANGE,
+        uid: 34
     },
+    // }],
     // tile 8
+    // ["35", {
     {
         face: FACES.FACE_D,
         "x-coordinate": 1,
         "y-coordinate": 2,
-        color: COLORS.ORANGE
+        color: COLORS.ORANGE,
+        uid: 35
     },
+    // }],
     // tile 9
+    // ["36", {
     {
         face: FACES.FACE_D,
         "x-coordinate": 2,
         "y-coordinate": 2,
-        color: COLORS.ORANGE
+        color: COLORS.ORANGE,
+        uid: 36
     },
+    // }],
 
     //======================================
     // FACE E (initially the right face)
@@ -323,74 +431,101 @@ const INIT_CONFIG = [
     // bottom row, left to right
     //--------------------------------------
     // tile 1
+    // ["37", {
     {
         face: FACES.FACE_E,
         "x-coordinate": 0,
         "y-coordinate": 0,
-        color: COLORS.YELLOW
+        color: COLORS.YELLOW,
+        uid: 37
     },
+    // }],
     // tile 2
+    // ["38", {
     {
         face: FACES.FACE_E,
         "x-coordinate": 1,
         "y-coordinate": 0,
-        color: COLORS.YELLOW
+        color: COLORS.YELLOW,
+        uid: 38
     },
+    // }],
     // tile 3
+    // ["39", {
     {
         face: FACES.FACE_E,
         "x-coordinate": 2,
         "y-coordinate": 0,
-        color: COLORS.YELLOW
+        color: COLORS.YELLOW,
+        uid: 39
     },
+    // }],
     //--------------------------------------
     // middle row, left to right
     //--------------------------------------
     // tile 4
+    // ["40", {
     {
         face: FACES.FACE_E,
         "x-coordinate": 0,
         "y-coordinate": 1,
-        color: COLORS.YELLOW
+        color: COLORS.YELLOW,
+        uid: 40
     },
+    // }],
     // tile 5
+    // ["41", {
     {
         face: FACES.FACE_E,
         "x-coordinate": 1,
         "y-coordinate": 1,
-        color: COLORS.YELLOW
+        color: COLORS.YELLOW,
+        uid: 41
     },
+    // }],
     // tile 6
+    // ["42", {
     {
         face: FACES.FACE_E,
         "x-coordinate": 2,
         "y-coordinate": 1,
-        color: COLORS.YELLOW
+        color: COLORS.YELLOW,
+        uid: 42
     },
+    // }],
     //--------------------------------------
     // top row, left to right
     //--------------------------------------
     // tile 7
+    // ["43", {
     {
         face: FACES.FACE_E,
         "x-coordinate": 0,
         "y-coordinate": 2,
-        color: COLORS.YELLOW
+        color: COLORS.YELLOW,
+        uid: 43
     },
+    // }],
     // tile 8
+    // ["44", {
     {
         face: FACES.FACE_E,
         "x-coordinate": 1,
         "y-coordinate": 2,
-        color: COLORS.YELLOW
+        color: COLORS.YELLOW,
+        uid: 44
     },
+    // }],
     // tile 9
+    // ["45", {
     {
         face: FACES.FACE_E,
         "x-coordinate": 2,
         "y-coordinate": 2,
-        color: COLORS.YELLOW
+        color: COLORS.YELLOW,
+        uid: 45
     },
+    // }],
 
     //======================================
     // FACE F (initially the left face)
@@ -400,74 +535,101 @@ const INIT_CONFIG = [
     // bottom row, left to right
     //--------------------------------------
     // tile 1
+    // ["46", {
     {
         face: FACES.FACE_F,
         "x-coordinate": 0,
         "y-coordinate": 0,
-        color: COLORS.WHITE
+        color: COLORS.WHITE,
+        uid: 46
     },
+    // }],
     // tile 2
+    // ["47", {
     {
         face: FACES.FACE_F,
         "x-coordinate": 1,
         "y-coordinate": 0,
-        color: COLORS.WHITE
+        color: COLORS.WHITE,
+        uid: 47
     },
+    // }],
     // tile 3
+    // ["48", {
     {
         face: FACES.FACE_F,
         "x-coordinate": 2,
         "y-coordinate": 0,
-        color: COLORS.WHITE
+        color: COLORS.WHITE,
+        uid: 48
     },
+    // }],
     //--------------------------------------
     // middle row, left to right
     //--------------------------------------
     // tile 4
+    // ["49", {
     {
         face: FACES.FACE_F,
         "x-coordinate": 0,
         "y-coordinate": 1,
-        color: COLORS.WHITE
+        color: COLORS.WHITE,
+        uid: 49
     },
+    // }],
     // tile 5
+    // ["50", {
     {
         face: FACES.FACE_F,
         "x-coordinate": 1,
         "y-coordinate": 1,
-        color: COLORS.WHITE
+        color: COLORS.WHITE,
+        uid: 50
     },
+    // }],
     // tile 6
+    // ["51", {
     {
         face: FACES.FACE_F,
         "x-coordinate": 2,
         "y-coordinate": 1,
-        color: COLORS.WHITE
+        color: COLORS.WHITE,
+        uid: 51
     },
+    // }],
     //--------------------------------------
     // top row, left to right
     //--------------------------------------
     // tile 7
+    // ["52", {
     {
         face: FACES.FACE_F,
         "x-coordinate": 0,
         "y-coordinate": 2,
-        color: COLORS.WHITE
+        color: COLORS.WHITE,
+        uid: 52
     },
+    // }],
     // tile 8
+    // ["53", {
     {
         face: FACES.FACE_F,
         "x-coordinate": 1,
         "y-coordinate": 2,
-        color: COLORS.WHITE
+        color: COLORS.WHITE,
+        uid: 53
     },
+    // }],
     // tile 9
+    // ["54", {
     {
         face: FACES.FACE_F,
         "x-coordinate": 2,
         "y-coordinate": 2,
-        color: COLORS.WHITE
+        color: COLORS.WHITE,
+        uid: 54
     },
+    // }],
 ];
 
 export default INIT_CONFIG;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import styled from "styled-components";
-import {configure, observable, action} from 'mobx';
+import {configure, observable, action, computed} from 'mobx';
 import {observer} from "mobx-react";
 import Face from "@components/face";
 import FACES from "@constants/faces";
@@ -70,16 +70,26 @@ const StyledUFace = styled(Face)`
 @observer
 class Cube extends React.Component {
     @observable title = "Rubik's Cube";
+    @observable ALL_TILES = INIT_CONFIG;
+    // @observable ALL_TILES = new Map(INIT_CONFIG);
+    @computed get FACE_A_TILES() { return this.ALL_TILES.filter(tile => tile.face === FACES.FACE_A); } // blue
+    @computed get FACE_B_TILES() { return this.ALL_TILES.filter(tile => tile.face === FACES.FACE_B); } // red
+    @computed get FACE_C_TILES() { return this.ALL_TILES.filter(tile => tile.face === FACES.FACE_C); } // green
+    @computed get FACE_D_TILES() { return this.ALL_TILES.filter(tile => tile.face === FACES.FACE_D); } // orange
+    @computed get FACE_E_TILES() { return this.ALL_TILES.filter(tile => tile.face === FACES.FACE_E); } // yellow
+    @computed get FACE_F_TILES() { return this.ALL_TILES.filter(tile => tile.face === FACES.FACE_F); } // white
 
     constructor(props) {
         super(props);
-        this.FACE_A_TILES = INIT_CONFIG.filter(tile => tile.face === FACES.FACE_A); // blue
-        this.FACE_B_TILES = INIT_CONFIG.filter(tile => tile.face === FACES.FACE_B); // red
-        this.FACE_C_TILES = INIT_CONFIG.filter(tile => tile.face === FACES.FACE_C); // green
-        this.FACE_D_TILES = INIT_CONFIG.filter(tile => tile.face === FACES.FACE_D); // orange
-        this.FACE_E_TILES = INIT_CONFIG.filter(tile => tile.face === FACES.FACE_E); // yellow
-        this.FACE_F_TILES = INIT_CONFIG.filter(tile => tile.face === FACES.FACE_F); // white
-        window.faceA = this.FACE_A_TILES;
+        // this.FACE_A_TILES = INIT_CONFIG.filter(tile => tile.face === FACES.FACE_A); // blue
+        // this.FACE_B_TILES = INIT_CONFIG.filter(tile => tile.face === FACES.FACE_B); // red
+        // this.FACE_C_TILES = INIT_CONFIG.filter(tile => tile.face === FACES.FACE_C); // green
+        // this.FACE_D_TILES = INIT_CONFIG.filter(tile => tile.face === FACES.FACE_D); // orange
+        // this.FACE_E_TILES = INIT_CONFIG.filter(tile => tile.face === FACES.FACE_E); // yellow
+        // this.FACE_F_TILES = INIT_CONFIG.filter(tile => tile.face === FACES.FACE_F); // white
+        // window.INIT_CONFIG = INIT_CONFIG;
+        // window.faceA = this.FACE_A_TILES;
+        window.ALL_TILES = this.ALL_TILES;
     }
 
     @action.bound
@@ -89,6 +99,33 @@ class Cube extends React.Component {
         } else {
             this.title = "Rubik's Cube";
         }
+    };
+
+    @action.bound
+    rotateMiddleRight90 = () => {
+        // top (C) and bottom (D) stay same
+        // front (A) -> right (E) -> back (B) -> left (F) -> front (A)
+        // middle tiles have y-coordinate = 1
+        let affectedTiles = this.ALL_TILES.filter(tile => tile["y-coordinate"] === 1 && tile.face !== FACES.FACE_C && tile.face !== FACES.FACE_D);
+        window.affectedTiles = affectedTiles;
+        affectedTiles.forEach(tile => {
+            switch(tile.face) {
+                case FACES.FACE_A:
+                    tile.face = FACES.FACE_E;
+                    break;
+                case FACES.FACE_E:
+                    tile.face = FACES.FACE_B;
+                    break;
+                case FACES.FACE_B:
+                    tile.face = FACES.FACE_F;
+                    break;
+                case FACES.FACE_F:
+                    tile.face = FACES.FACE_A;
+                    break;
+                default:
+                    throw new Error("oops mistake made");
+            }
+        });
     };
 
     render() {
@@ -111,6 +148,12 @@ class Cube extends React.Component {
                     <StyledDFace tiles={this.FACE_E_TILES} />
                     <StyledUFace tiles={this.FACE_F_TILES} />
                 </StyledGrid>
+                <br />
+                <button
+                    onClick={this.rotateMiddleRight90}
+                >
+                    Rotate Middle =>
+                </button>
             </React.Fragment>
         );
     }
